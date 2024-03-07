119c6ebfa91911693333548f2bacdf3838e6321d
msolis488 <lovedotmarketing@gmail.com>
2024-03-07 12:15:50 -0600

    More Rotation Adjustments
    More Rotation Adjustments

--- src/TargetTracker.jsx
+++ src/TargetTracker.jsx
@@ -38,6 +38,7 @@ export default () => {
 
       model.scale.set(0.1, 0.1, 0.1);
       model.rotation.x = Math.PI / 2;
+      model.rotation.z = Math.PI / 2;
 
       // Apply steel-like material
       model.traverse((child) => {
